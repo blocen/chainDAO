599f6892afa1d6fb62a54cfb2a1536e9ff4c8ffc
Raphael <89421168+blocen@users.noreply.github.com>
2021-11-25 16:42:05 +0100

    GET on a snapshot proposal

---
+++ index.js
@@ -0,0 +1,91 @@
+const { Requester, Validator } = require('@chainlink/external-adapter')
+
+// Define custom error scenarios for the API.
+// Return true for the adapter to retry.
+const customError = (data) => {
+  if (data.Response === 'Error') return true
+  return false
+}
+
+// Define custom parameters to be used by the adapter.
+// Extra parameters can be stated in the extra object,
+// with a Boolean value indicating whether or not they
+// should be required.
+const customParams = {
+  proposal_id: ['proposal_id'],
+  endpoint: false
+}
+
+// curl -X GET -H "content-type:application/json" "http://localhost:8080/" --data '{"data":{"proposal_id":"0xd810c4cf2f09737a6f833f1ec51eaa5504cbc0afeeb883a21a7e1c91c8a597e4"}}'
+
+const createRequest = (input, callback) => {
+  // The Validator helps you validate the Chainlink request data
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+
+  // const proposal_id = "0xd810c4cf2f09737a6f833f1ec51eaa5504cbc0afeeb883a21a7e1c91c8a597e4"
+  const proposal_id = validator.validated.data.proposal_id
+  const url = `https://hub.snapshot.org/graphql?query=%0A%0Aquery%20Proposal%20%7B%0A%09proposal%20(id%3A%22${proposal_id}%22)%20%7B%0A%09%09votes%0A%20%20%20%20scores%0A%20%20%20%20scores_by_strategy%0A%20%20%20%20scores_state%0A%20%20%20%20scores_total%0A%20%20%20%20scores_updated%0A%20%20%7D%0A%7D%0A`
+
+  const params = {
+    proposal_id
+  }
+
+  // This is where you would add method and headers
+  // you can add method like GET or POST and add it to the config
+  // The default is GET requests
+  // method = 'get' 
+  // headers = 'headers.....'
+  const config = {
+    url,
+    params
+  }
+
+  // The Requester allows API calls be retry in case of timeout
+  // or connection failure
+  Requester.request(config, customError)
+    .then(response => {
+      // It's common practice to store the desired value at the top-level
+      // result key. This allows different adapters to be compatible with
+      // one another.
+      // response.data.result = Requester.validateResultNumber(response.data, [tsyms])
+
+      console.log("we got votes:", response)
+      callback(response.status, Requester.success(jobRunID, response))
+    })
+    .catch(error => {
+      callback(500, Requester.errored(jobRunID, error))
+    })
+}
+
+// This is a wrapper to allow the function to work with
+// GCP Functions
+exports.gcpservice = (req, res) => {
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// AWS Lambda
+exports.handler = (event, context, callback) => {
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// newer AWS Lambda implementations
+exports.handlerv2 = (event, context, callback) => {
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
+    })
+  })
+}
+
+// This allows the function to be exported for testing
+// or for running in express
+module.exports.createRequest = createRequest
